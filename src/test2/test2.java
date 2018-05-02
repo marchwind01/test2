cfbbf3ecc0f520b7dd79ad4fe0fbb6374ba145f2
LYC <14903479@qq.com>
2018-05-02 08:24:06 +0800

    test2

--- src/test2/test2.java
+++ src/test2/test2.java
@@ -6,7 +6,7 @@ public class test2
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-        System.out.println("hello world");
+        System.out.println("hello world!!!");
 	}
 
 }
